45665f6ac89c6cbf5d746e65183eb31f8db8f5c9
Erik Ivan Melo <megaerikivan2@gmail.com>
2020-11-09 20:00:47 -0300

    Added some font functions

--- cliesc.h
+++ cliesc.h
@@ -3,6 +3,8 @@
 
 #include <stdio.h>
 
+/* CURSOR FUNCTIONS */
+
 /* Moves the cursor n up */
 #define cup(n)		 printf("\033[%iA",n);
 
@@ -38,5 +40,63 @@
 
 /* Hide cursor */
 #define chide()		 printf("\033[?25l");
+
+
+/* FONT FUNCTIONS */
+
+/* Reset the font attribute */
+#define freset()		printf("\033[0m");
+
+/* Turn on the bold or increased intensity */
+#define fbold()			printf("\033[1m");
+
+/* Turn on the faint or decreased intensity */
+#define ffaint()		printf("\033[2m");
+
+/* Turn on the italic (Sometimes treated as inverse or blink) [not widely supported] */
+#define fitalic()		printf("\033[3m");
+
+/* Turn on the underline */
+#define funderline()	printf("\033[4m");
+
+/* Turn on the blinking */
+#define fblink()		printf("\033[5m");
+
+/* Turn on the rapid blinking [not widely supported] */
+#define frblink()		printf("\033[6m");
+
+/* Swaps foreground and background colors, */
+#define frevers()		printf("\033[7m");
+
+/* Hides text */
+#define fhide()			printf("\033[8m");
+
+/* Turn on the strike through */
+#define fstrike()		printf("\033[9m");
+
+/* Turn on the double underline */
+#define fdunderline()	printf("\033[21m");
+
+/* Turn off the bold */
+#define fboldoff()		printf("\033[22m");
+
+/* Turn off the italic */
+#define fitalicoff()	printf("\033[23m");
+
+/* Turn off the underline */
+#define funderlineoff()	printf("\033[24m");
+
+/* Turn off the blinking */
+#define fblinkoff()		printf("\033[25m");
+
+/* Turn off the revers text */
+#define freversoff()	printf("\033[27m");
+
+/* Shows the text */
+#define fshow()			printf("\033[28m");
+
+/* Turn off the strike through */
+#define fsrikeoff()		printf("\033[29m");
+
 
 #endif
